b38f8d046140fa87fd724aa027c21682912f5b7c
Jhonatan Bezerra <jhonlisboa15@hotmail.com>
2022-09-07 16:25:58 -0300

    atualizaçãos

---
+++ src/app/ILista.ts
@@ -0,0 +1,10 @@
+export interface ILista{
+    activity_amount: number,
+	average_duration_per_case: string,
+	average_events_per_case: number,
+	case_amount: number,
+	event_amount: number,
+	events_per_case: number,
+	median_duration: string,
+	variant_amount: number,
+}
